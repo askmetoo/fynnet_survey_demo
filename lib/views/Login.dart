437c336d3ae999e2d788ac16bf0384c3981bfc45
Krit Petrachaianan <krit.petra@gmail.com>
2020-02-19 01:45:37 +0700

    enhanced login dialog with signup toggle

--- lib/views/Login.dart
+++ lib/views/Login.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginDialog extends StatelessWidget{
+class LoginDialog extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoginDialogState();
+}
+
+class _LoginDialogState extends State<LoginDialog>{
   final TextEditingController _usernameController = new TextEditingController();
   final TextEditingController _passwordController = new TextEditingController();
+
+  bool _signupAction = false;
 
   @override 
   Widget build(BuildContext context) {
@@ -11,10 +18,10 @@ class LoginDialog extends StatelessWidget{
       contentPadding: EdgeInsets.all(24),
 
       children: [
-        // Login Text
+        // Log In / Sign Up Text
         Padding(
           padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-          child: Text('Log in to save and customize your experience',
+          child: Text( _signupAction ? 'Sign up below to create your own surveys!' : 'Log in to save and customize your experience',
             textAlign: TextAlign.center,
             style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18)
           )
@@ -32,7 +39,7 @@ class LoginDialog extends StatelessWidget{
               border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
             ),
 
-            validator: (String value) => value.isEmpty ? 'Username cannot be empty.' : null
+            //validator: (String value) => value.isEmpty ? 'Username cannot be empty.' : null
           )
         ),
 
@@ -48,15 +55,15 @@ class LoginDialog extends StatelessWidget{
               border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
             ),
 
-            validator: (String value) => value.isEmpty ? 'Password cannot be empty.' : null
+            //validator: (String value) => value.isEmpty ? 'Password cannot be empty.' : null
           )
         ),
 
-        // Login Button
+        // Login / Sign Up Button
         Padding(
           padding: EdgeInsets.all(2.0),
           child: RaisedButton(
-            child: Text('Log In'),
+            child: _signupAction ? Text('Sign Up') : Text('Log In'),
             color: Colors.green[700],
             textColor: Colors.white,
             padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
@@ -64,6 +71,22 @@ class LoginDialog extends StatelessWidget{
             onPressed: () {
               // TODO
               print('${_usernameController.text} | ${_passwordController.text}');
+            }
+          )
+        ),
+
+        // Toggle between Log In / Sign Up forms
+        Padding(
+          padding: EdgeInsets.all(2.0),
+          child: FlatButton(
+            child: _signupAction ? Text('Have an account? Log in here.') : Text('Sign up for an account'),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
+            onPressed: () {
+              setState(() {
+                // TODO: clear form
+                _signupAction = !_signupAction;
+              });
+              
             }
           )
         )
